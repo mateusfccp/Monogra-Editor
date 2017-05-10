8725408b377f1506548e05a2f6ff0d210bc310b6
UNASP-EC - Mateus Felipe Cordeiro Caetano Pinto <mateusfccp@gmail.com>
2017-05-10 17:49:36 -0300

    Sass support added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 
 var concat = require('gulp-concat');
-var elm = require('gulp-elm');
 var cleanCSS = require('gulp-clean-css');
+var elm = require('gulp-elm');
 var nodemon = require('gulp-nodemon');
 var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
 
 // Elm related
 
@@ -29,26 +31,40 @@ gulp.task('elm-bundle', ['elm-init'], function() {
 
 // CSS related
 
-gulp.task('css-bundle', function() {
-    return gulp.src('src/css/**/*.css')
+gulp.task('sass', function() {
+    return gulp.src('src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('css', ['sass'], function() {
+    return gulp.src(['src/css/**/reboot.css', 'src/css/**/bascss.min.css', 'src/css/**/*.css'])
         .pipe(concat('style.css'))
         .pipe(cleanCSS())
         .pipe(gulp.dest('public/dist/'));
 });
 
-gulp.task('build', ['elm-bundle', 'css-bundle']);
+gulp.task('build-css', function(callback) {
+    runSequence('sass', 'css');
+});
+
+gulp.task('build', ['elm-bundle', 'build-css']);
 
 gulp.task('serve-json', function() {
     nodemon({ script: 'api.js' });
 })
 
 gulp.task('debug-server', ['build', 'serve-json'], function() {
-    var elm_watcher = gulp.watch('src/elm/**/*.elm', ['elm-bundle'])
-    var css_watcher = gulp.watch('src/css/**/*.css', ['css-bundle']);
+    var elm_watcher = gulp.watch('src/elm/**/*.elm', ['elm-bundle']);
+    var css_watcher = gulp.watch('src/css/**/*.css', ['build-css']);
+    var sass_watcher = gulp.watch('src/sass/**/*.scss', ['build-css']);
     elm_watcher.on('change', function(event) {
         console.log('Elm Watcher: File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
     css_watcher.on('change', function(event) {
         console.log('CSS Watcher: File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+    sass_watcher.on('change', function(event) {
+        console.log('Sass Watcher: File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 })
