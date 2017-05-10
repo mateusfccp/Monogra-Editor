c7dc06ee6831083b6249d387d86b6535f56c4a51
UNASP-EC - Mateus Felipe Cordeiro Caetano Pinto <mateusfccp@gmail.com>
2017-05-10 16:56:02 -0300

    Updated dependencies, changed gulp-minify-css to gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,48 @@
 var gulp = require('gulp');
 
 var concat = require('gulp-concat');
-var elm	= require('gulp-elm');
-var minifyCSS = require('gulp-minify-css');
+var elm = require('gulp-elm');
+var cleanCSS = require('gulp-clean-css');
 var nodemon = require('gulp-nodemon');
-var rename = require ('gulp-rename');
+var rename = require('gulp-rename');
 
 // Elm related
 
 gulp.task('elm-init', elm.init);
 
-gulp.task('elm', ['elm-init'], function(){
+gulp.task('elm', ['elm-init'], function() {
     return gulp.src('src/elm/**/*.elm')
-               .pipe(elm())
-               .pipe(gulp.dest('public/dist/'));
+        .pipe(elm())
+        .pipe(gulp.dest('public/dist/'));
 });
 
-gulp.task('elm-bundle', ['elm-init'], function(){
+gulp.task('elm-bundle', ['elm-init'], function() {
     return gulp.src('src/elm/**/*.elm')
-               .pipe(elm.bundle('bundle.js'))
-               .on('error', function (error) {
-                   console.log(error.toString());
-                   this.emit('end');
-               })
-               .pipe(rename('app.js'))
-               .pipe(gulp.dest('public/dist/'));
+        .pipe(elm.bundle('bundle.js'))
+        .on('error', function(error) {
+            console.log(error.toString());
+            this.emit('end');
+        })
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public/dist/'));
 });
 
 // CSS related
 
-gulp.task('css-bundle', function () {
+gulp.task('css-bundle', function() {
     return gulp.src('src/css/**/*.css')
-               .pipe(concat('style.css'))
-               .pipe(minifyCSS())
-               .pipe(gulp.dest('public/dist/'));
+        .pipe(concat('style.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('public/dist/'));
 });
 
 gulp.task('build', ['elm-bundle', 'css-bundle']);
 
-gulp.task ('serve-json', function () {
+gulp.task('serve-json', function() {
     nodemon({ script: 'api.js' });
 })
 
-gulp.task('debug-server', ['build', 'serve-json'], function () {
+gulp.task('debug-server', ['build', 'serve-json'], function() {
     var elm_watcher = gulp.watch('src/elm/**/*.elm', ['elm-bundle'])
     var css_watcher = gulp.watch('src/css/**/*.css', ['css-bundle']);
     elm_watcher.on('change', function(event) {
