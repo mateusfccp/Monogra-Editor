90cad4f03f2bd45a7c367a65b957ed0dfef07ec1
UNASP-EC - Mateus Felipe Cordeiro Caetano Pinto <mateusfccp@gmail.com>
2017-05-17 17:11:36 -0300

    server compression

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var jsonServer = require('json-server');
-var express = require('express');
+var compression = require('compression');
 var cors = require('cors');
+var express = require('express');
+var jsonServer = require('json-server');
 
 // Return a express server
 var server = express();
 
+server.use(compression());
 server.use('/', express.static('public'));
 
 // Set default middlewares (logger, static, cors and no-cache)
